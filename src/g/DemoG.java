695d45f7c23e6585931a9748cafb26e74fdbe7aa
Samuel <Samuel@192.168.0.21>
2024-01-05 15:29:46 -0800

    initial commit

---
+++ src/g/DemoG.java
@@ -0,0 +1,11 @@
+package g;
+
+public class DemoG {
+	public static void main(String[] args) {
+		System.out.println("this is homework ");
+		
+		System.out.println("ali was in here ");
+		System.out.println("2024 is here  ");
+	}
+
+}
